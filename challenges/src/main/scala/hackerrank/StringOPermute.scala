7b00efa13ef1d4a94a8031a59ee21dcbdf909606
Naveen <dexter2305@gmail.com>
2022-02-10 21:46:39 +0530

    Fixed tests

--- challenges/src/main/scala/hackerrank/StringOPermute.scala
+++ challenges/src/main/scala/hackerrank/StringOPermute.scala
@@ -4,10 +4,14 @@ object StringOPermute extends App {
 
   def stringOPermute(s: String): String = (for (i <- 0 until s.length() by 2) yield (s.substring(i, i + 2).reverse)).mkString
 
+  println(s"'${stringOPermute("")}'")
+
+  
   for (_ <- 0 until scala.io.StdIn.readInt()) {
     val t = scala.io.StdIn.readLine()
     val o = stringOPermute(t)
     println(o)
   }
+  
 
 }
--- challenges/src/test/scala/hackerrank/StringOPermuteSpec.scala
+++ challenges/src/test/scala/hackerrank/StringOPermuteSpec.scala
@@ -4,29 +4,35 @@ import org.scalatest.propspec.AnyPropSpec
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import org.scalatest.matchers.should.Matchers
 import org.scalacheck.Gen
+import org.scalactic.anyvals.PosInt
 
 class StringOPermuteSpec extends AnyPropSpec with ScalaCheckPropertyChecks with Matchers {
+
+  override def minSuccessful(value: PosInt): MinSuccessful = MinSuccessful(500)
 
   val evenLengthStringGen =
     Gen.alphaNumStr.map { s =>
       s.length match {
         case 0 => s
         case l if l % 2 == 0 => s
-        case l if l % 2 == 1 => s"$s${Gen.alphaNumChar}"
+        case l if l % 2 == 1 => s"${s.substring(0, s.length - 1)}"
       }
     }
 
   property("length of permutted string is always equal to given string") {
-    forAll(evenLengthStringGen) { s: String =>
-      StringOPermute.stringOPermute(s).length() should be(s.length())
+    forAll((evenLengthStringGen, "string")) { s: String =>
+      if (s.isEmpty) {
+        println("*** empty string ")
+        StringOPermute.stringOPermute(s) should be(s)
+      } else StringOPermute.stringOPermute(s).length() should be(s.length())
     }
   }
 
   property("permuting permuted string should return given string") {
-    forAll(evenLengthStringGen) { s =>
+    forAll((evenLengthStringGen, "string")) { s =>
       val permuted  = StringOPermute.stringOPermute(s)
       val expectedS = StringOPermute.stringOPermute(permuted)
-      expectedS should ===(s)
+      expectedS should be(s)
     }
   }
 }
