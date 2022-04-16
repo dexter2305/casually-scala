a47687f667a96844f9fea54a50604bdd5d8e6aa0
Naveen <dexter2305@gmail.com>
2022-04-16 13:52:06 +0530

    removed unuused test and more hackerrank challenges

---
+++ challenges/src/main/scala/hackerrank/FunctionOrNot.scala
@@ -0,0 +1,18 @@
+package hackerrank
+
+import scala.io.StdIn
+
+object FunctionOrNot extends App {
+
+  for (test <- 0 until StdIn.readInt()) {
+    val intPairSeq = for {
+      elementCount <- 0 until StdIn.readInt()
+      data        = StdIn.readLine()
+      stringArray = (data.split(" "))
+      intPair     = (stringArray(0).toInt, stringArray(1).toInt)
+    } yield intPair
+    if (isFunction(intPairSeq)) println("YES") else println("NO")
+  }
+
+  def isFunction(xform: Seq[(Int, Int)]): Boolean = !xform.groupBy(_._1).exists(groups => groups._2.length > 1)
+}
