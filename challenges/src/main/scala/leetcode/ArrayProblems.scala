fa03be2ee351a5992625b1684c12d6f7d66474af
Naveen <dexter2305@gmail.com>
2022-05-13 02:31:52 +0530

    day 3 of leetcode dsa

--- challenges/src/main/scala/leetcode/ArrayProblems.scala
+++ challenges/src/main/scala/leetcode/ArrayProblems.scala
@@ -1,9 +1,48 @@
 package leetcode
 
 object ArrayProblems extends App {
-  val l = Array(4, 5, 6, 0, 0, 0)
-  merge(l, 3, Array(1, 2, 3), 3)
-  println(l.mkString(" "))
+
+  assert(ArrayProblems.maxProfit(Array(7, 1, 5, 3, 6, 4)) == 5)
+  //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+  /*
+      => track min element and profit
+         newMin = min (curMin, currentElement)
+         newProfit = max (curProfit, currentElement - newMin)
+
+      t: O(n)
+      s: O(1)
+
+      => In a way this is application of Kadane's algorithm.
+         Prices will never be negative which is same as max sum subarray with all positives.
+         In this case, differential price gives negatives which is passed through kadanes' algorithm
+      val differentialPrice = prices.sliding(2).toList.map(array => array(1)- array(0))
+      kadaneAlgo(differentialPrice)
+   */
+  def maxProfit(prices: Array[Int]): Int = {
+    val rMin        = Int.MaxValue
+    val rProfit     = 0
+    val (_, profit) = prices.foldLeft((rMin, rProfit)) { case ((rMin, rProfit), e) =>
+      val newMin    = math.min(rMin, e)
+      val newProfit = math.max(rProfit, e - newMin)
+      (newMin, newProfit)
+    }
+    profit
+  }
+
+  // https://leetcode.com/problems/intersection-of-two-arrays-ii/
+  def intersect(nums1: Array[Int], nums2: Array[Int]): Array[Int] = {
+
+    @scala.annotation.tailrec
+    def aux(nums1: Array[Int], nums2: Array[Int], i: Int, j: Int, acc: Array[Int], k: Int): Array[Int] = {
+      if (i == nums1.length || j == nums2.length) acc
+      else if (nums1(i) < nums2(j)) aux(nums1, nums2, i + 1, j, acc, k)
+      else if (nums1(i) > nums2(j)) aux(nums1, nums2, i, j + 1, acc, k)
+      else // nums1(i) must be == nums2(j)
+        aux(nums1, nums2, i + 1, j + 1, acc :+ nums1(i), k + 1)
+    }
+    aux(nums1.sorted, nums2.sorted, 0, 0, Array.emptyIntArray, 0)
+  }
+
   // https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
   def finalValueAfterOperations(operations: Array[String]): Int = {
     operations.foldLeft(0) { (s, op) =>
@@ -18,6 +57,7 @@ object ArrayProblems extends App {
   //traverse reverse
   def merge(nums1: Array[Int], m: Int, nums2: Array[Int], n: Int): Unit = {
 
+    @scala.annotation.tailrec
     def sortMerge(left: Array[Int], right: Array[Int], l: Int, r: Int, ri: Int): Array[Int] = {
       //println(s"${left.mkString(" ")} | ${right.mkString(" ")}")
       (l, r) match {
---
+++ challenges/src/test/scala/leetcode/ArrayProblemsSpec.scala
@@ -0,0 +1,127 @@
+package leetcode
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+import leetcode._
+class ArrayProblemsSpec extends AnyFreeSpec with Matchers {
+
+  "Merge sorted arrays" - {
+    "when left has smallest element" in {
+      val left      = Array(1, 2, 3, 0, 0, 0)
+      val leftSize  = 3
+      val right     = Array(8, 9, 12)
+      val rightSize = 3
+
+      val leftClone      = left.take(leftSize)
+      val sortedExpected = (leftClone ++ right).sorted
+
+      ArrayProblems.merge(left, leftSize, right, rightSize)
+      left should equal(sortedExpected)
+    }
+    "when right has smallest element" in {
+      val left      = Array(100, 200, 349, 0, 0, 0)
+      val leftSize  = 3
+      val right     = Array(8, 9, 12)
+      val rightSize = 3
+
+      val leftClone      = left.take(leftSize)
+      val sortedExpected = (leftClone ++ right).sorted
+
+      ArrayProblems.merge(left, leftSize, right, rightSize)
+      left should equal(sortedExpected)
+    }
+    "when right array is smaller than left array" in {
+      val left      = Array(100, 200, 349, 0)
+      val leftSize  = 3
+      val right     = Array(12)
+      val rightSize = 1
+
+      val leftClone      = left.take(leftSize)
+      val sortedExpected = (leftClone ++ right).sorted
+
+      ArrayProblems.merge(left, leftSize, right, rightSize)
+      left should equal(sortedExpected)
+    }
+    "when left array is smaller than right array" in {
+      val left      = Array(100, 0, 0, 0)
+      val leftSize  = 1
+      val right     = Array(120, 420, 900)
+      val rightSize = 3
+
+      val leftClone      = left.take(leftSize)
+      val sortedExpected = (leftClone ++ right).sorted
+
+      ArrayProblems.merge(left, leftSize, right, rightSize)
+      left should equal(sortedExpected)
+    }
+    "when both arrays are empty" in {
+      val left  = Array.emptyIntArray
+      val right = Array.emptyIntArray
+      ArrayProblems.merge(left, 0, right, 0)
+      left should equal(Array.emptyIntArray)
+    }
+    "when right array is empty" in {
+      val left     = Array(1, 2, 3, 0, 0, 0)
+      val leftSize = 3
+      val right    = Array.emptyIntArray
+      ArrayProblems.merge(left, leftSize, right, 0)
+      left should equal(left)
+    }
+    "when left array is empty but has placeholders for right array" in {
+      val left      = Array(0, 0, 0)
+      val leftSize  = 0
+      val right     = Array(1, 3, 4)
+      val rightSize = 3
+      ArrayProblems.merge(left, leftSize, right, rightSize)
+      left should equal(right)
+    }
+  }
+
+  "Intersection of two arrays II" - {
+    "when arrays are of same size" - {
+      "when two arrays have no intersections" in {
+        val left  = Array(1, 2, 3)
+        val right = Array(4, 5, 6)
+        ArrayProblems.intersect(left, right) should equal(Array())
+      }
+      "when multiple distinct ints intersect" in {
+        val left  = Array(3, 4, -5, 7, -2)
+        val right = Array(10, 11, 4, -1, 7)
+        ArrayProblems.intersect(left, right) should contain theSameElementsAs (Array(7, 4))
+      }
+      "when multiple identical ints intersect" in {
+        val left = Array(8, 9, -1, 8)
+        val right = Array(27, 8, 8, 9)
+      }
+      "when both arrays are empty" in {
+        ArrayProblems.intersect(Array.emptyIntArray, Array.emptyIntArray) should be (Array.emptyIntArray)
+      }
+    }
+    "when arrays are varying size" - {
+      "when left array is empty" in {
+        ArrayProblems.intersect(Array.emptyIntArray, Array(9,10,11)) should be (Array.emptyIntArray)
+      }
+      "when right array is empty" in {
+        ArrayProblems.intersect(Array(7, 2, 1), Array.emptyIntArray) should be (Array.emptyIntArray)
+      }
+      "when distinct ints intersect" in {
+        val left = Array(1, 2, 3, 4, 5)
+        val right = Array(3, 2, 5)
+        ArrayProblems.intersect(left, right) should be (Array(2, 3, 5))
+      }
+    }
+  }
+
+  "maxProfit" - {
+    "should be profit when prices increase constantly" in {
+      ArrayProblems.maxProfit(Array(1,2,3)) should be (2)
+    }
+    "should be no profit when prices decrease constantly" in {
+      ArrayProblems.maxProfit(Array(7,6,5,4,3)) should be (0)
+    }
+    "profit when early peaks with drops" in {
+      ArrayProblems.maxProfit(Array(7,1,5,3,6,4)) should be (5)
+    }
+  }
+}
