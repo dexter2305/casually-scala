e9dcd59e39706ea5d7643fa2cb9c1feb0d25244e
Naveen <dexter2305@gmail.com>
2022-05-14 15:44:12 +0530

    reshape matrix

--- challenges/src/test/scala/leetcode/ArrayProblemsSpec.scala
+++ challenges/src/test/scala/leetcode/ArrayProblemsSpec.scala
@@ -12,6 +12,33 @@ import org.scalatest.time.Seconds
 
 class ArrayProblemsSpec extends AnyFreeSpec with Matchers {
 
+  "Reshape matrix" - {
+    "reshape not possible" - {
+      "given matrix < expected" in {
+        val input = Array(Array(1,2,3), Array(4,5,6))
+        ArrayProblems.matrixReshape(input, 20, 30) should be (input)
+      }
+      "given matrix > expected" in {
+        val input = Array(Array(1,2,3), Array(4,5,6))
+        ArrayProblems.matrixReshape(input, 2, 2) should be (input)
+      }      
+    }
+    "reshape possible" - {
+      "2 x 3 => 3 x 2" in {
+        val input = Array(Array(1,2,3), Array(4,5,6))
+        ArrayProblems.matrixReshape(input, 3, 2) should be (Array(Array(1,2), Array(3,4), Array(5,6)))
+
+      }
+      "1 x 4 => 2 x 2" in {
+        val input = Array(Array(1, 2, 3, 4))
+        ArrayProblems.matrixReshape(input, 2, 2) should be (Array(Array(1, 2), Array(3, 4)))
+      }
+      "3 x 3 => 1 x 9" in {
+        val input = Array(Array(1,2,3), Array(4,5,6), Array(7,8,9))
+        ArrayProblems.matrixReshape(input, 1, 9) should be (Array(Array(1,2,3,4,5,6,7,8,9)))
+      }
+    }
+  }
   "Convert 1d array to 2d array" - {
     "when transformation not possible" - {
       "return empty array" - {
