1c4264be56fc4db44e18d8f3d9fda07949944ac3
Naveen <dexter2305@gmail.com>
2022-05-21 22:01:06 +0530

    Level order traversal

--- challenges/src/main/scala/leetcode/BinaryTreeTraversal.scala
+++ challenges/src/main/scala/leetcode/BinaryTreeTraversal.scala
@@ -49,18 +49,33 @@ object BinaryTreeTraversal extends App {
   //https://leetcode.com/problems/binary-tree-level-order-traversal/
   def levelOrder(root: TreeNode): List[List[Int]] = {
 
-    def loop(nodes: List[TreeNode]): List[List[Int]] = {
-      nodes match {
-        case null => List.empty
-        case Nil  => List.empty
-        case _    =>
-          nodes.filter(_ != null).map(_.value) ::: loop(
-            nodes.filter(_ != null).flatMap(node => List(node.left, node.right)).filter(_ != null),
-          )
+    @scala.annotation.tailrec
+    def go(current: List[TreeNode], acc: List[List[Int]]): List[List[Int]] = {
+      //every pass-through of go accumulates two items
+      // 1. List of all values in a particular level of binary tree
+      // 2. List of all child nodes in the same level of binary tree. This becomes input to the next recursive call until input becomes empty (visited all child nodes)
+      //
+      current match {
+        case head :: Nil if head == null => /*println("when input is null.. ") ;*/ acc
+        case Nil                         => acc
+        case listOfNodes                 =>
+          // aggregate (v to list of child nodes - this is binary tree, child node count can vary from 0-2)
+          val aggregation: List[(Int, List[TreeNode])] = (for (node <- listOfNodes)
+            yield (node.value, (node.left, node.right))).map {
+            case (v, (null, null)) => (v, List.empty)
+            case (v, (l, null))    => (v, List(l))
+            case (v, (null, r))    => (v, List(r))
+            case (v, (l, r))       => (v, List(l, r))
+          }
+          //println(s"${aggregation.mkString(" ")}")
+          val (currentValues, nextLevels)              = aggregation.foldLeft((List.empty[Int], List.empty[TreeNode])) { case (acc, element) =>
+            (acc._1 :+ element._1, acc._2 ::: element._2)
+          }
+
+          go(nextLevels, acc.appended(currentValues))
       }
     }
 
-    val r = loop(List(root))
-    r
+    go(List(root), List.empty)
   }
 }
