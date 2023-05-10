eb8980b940ec477fec96ee7211faa745a7fcdfa9
Naveen <itsnashter@pm.me>
2023-05-10 19:11:23 +0530

    Refactored

--- challenges/src/test/scala/neetcode/ArraysAndHashingSpec.scala
+++ challenges/src/test/scala/neetcode/ArraysAndHashingSpec.scala
@@ -2,7 +2,8 @@ package neetcode
 
 import basetypes.UnitTest
 import ArraysAndHashing._
-class ArraysAndHashingSpec extends UnitTest("ArrayAndHashing") {
+import org.scalatest.propspec.AnyPropSpec
+class ArraysAndHashingSpec extends UnitTest("ArrayAndHashing")  {
 
   "contains duplicate" must "return true for Array[1,2,3,1]" in { containsDuplicate(Array(1, 2, 3, 1)) must be(true) }
   it must "return false for Array[1,2,3,4]" in { containsDuplicate(Array(1, 2, 3, 4)) must be(false) }
